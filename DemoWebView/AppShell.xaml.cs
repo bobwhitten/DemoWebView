77645ce93a91086775022fc0967509e3179846a1
bobw <bobw@BOBWHITTEN29FD>
2024-12-23 17:15:57 -0800

    Add project files.

---
+++ DemoWebView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoWebView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DemoWebView/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace DemoWebView
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void WebViewBtn_Clicked(object sender, EventArgs e) {
+            myMediaFrame.IsVisible = !myMediaFrame.IsVisible;
+            if (myMediaFrame.IsVisible) {
+                myWebView.Source = new UrlWebViewSource
+                {
+                    Url = $"https://en.m.wikipedia.org/wiki/{count}"
+                };
+
+            }
+        }
+    }
+
+}
